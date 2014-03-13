c53a06879f2b94235e8ae1cb2a318dbd084798b2
João Melo <jmelo@lyncode.com>
2014-03-13 20:21:47 +0000

    Added a simple example for an app with inheritance

---
+++ jtwig-simpleapp-with-inheritance/src/main/java/com/lyncode/jtwig/example/SampleApp.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright 2012 Lyncode
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.lyncode.jtwig.example;
+
+import com.lyncode.jtwig.JtwigContext;
+import com.lyncode.jtwig.JtwigTemplate;
+import com.lyncode.jtwig.exception.CompileException;
+import com.lyncode.jtwig.exception.ParseException;
+import com.lyncode.jtwig.exception.RenderException;
+import com.lyncode.jtwig.resource.ClasspathJtwigResource;
+
+public class SampleApp {
+    public static void main (String... args) throws ParseException, CompileException, RenderException {
+        JtwigTemplate template = new JtwigTemplate(new ClasspathJtwigResource("/views/index/sample.twig"));
+        String result = template.output(new JtwigContext().withModelAttribute("name", "Jtwig"));
+        System.out.println(result);
+    }
+}
